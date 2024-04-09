2d348d82265d45867ef020f678fd794d33afbfc7
Angel-HF <49835792+Pelochus@users.noreply.github.com>
2024-04-09 19:51:09 +0200

    Added git-lfs

--- docker/setup.sh
+++ docker/setup.sh
@@ -2,7 +2,7 @@
 
 # Dependencies
 apt update
-apt install -y python3 pip git git-lfs curl wget nano sudo # python3-tk
+apt install -y python3 pip git curl wget nano sudo 
 
 # Better than using the default from Rockchip
 curl https://raw.githubusercontent.com/Pelochus/ezrknn-llm/master/install.sh | sudo bash
@@ -14,6 +14,9 @@ pip install /ezrknn-llm/rkllm-toolkit/packages/rkllm_toolkit-1.0.0-cp38-cp38-lin
 cd /ezrknn-llm/rkllm-toolkit/examples/huggingface
 git clone https://huggingface.co/Qwen/Qwen-1_8B-Chat
 git clone https://huggingface.co/georgesung/llama2_7b_chat_uncensored
+
+# Done here to avoid cloning full repository for the Docker image
+apt install -y git-lfs # python3-tk
 
 # cd Qwen-1_8B-Chat
 # git lfs pull
